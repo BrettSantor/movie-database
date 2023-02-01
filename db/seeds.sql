40c6c4e767613f6fe06d220af4c5592a02ff6eea
Ashley Sese <sese.ashrean@gmail.com>
2023-02-01 14:20:40 -0800

    finished seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies(movie_name)
+VALUES
+('The Devil Wears Prada'),
+('KILL BILL'),
+('The Big Lebowski'),
+('Finding Dory');
+
+INSERT INTO reviews(review, movie_id)
+VALUES
+('Fantastic!', 1),
+('KILLER SCENES!!', 2),
+('It really ties the room together!', 3),
+('Wholesome, memorable!', 4);
